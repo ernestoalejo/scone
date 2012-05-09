9c436113c1b6aeae49e0220088c843d2d1e8b8e8
Santiago <elwarxy@gmail.com>
2012-05-09 20:32:05 +0200

    Cargador de imagenes completo.

--- include/resources-manager.h
+++ include/resources-manager.h
@@ -1,13 +1,25 @@
 #ifndef __RESOURCESMANAGER_H__
 #define __RESOURCESMANAGER_H__
 
+
+#include <SFML/Graphics.hpp>
+#include "common.h"
+#include <SFML/Audio.hpp>
+#include <iostream>
+
 class ResourcesManager{
 
 public:
-	void loadtextures(const sf::Sprite& imagen);
+	ResourcesManager();
+	void loadTextures(sf::Sprite& sprite, string name);
+	~ResourcesManager(void);
 
 private:
 	DISALLOW_COPY_AND_ASSIGN(ResourcesManager);
+
+	map<string, sf::Texture*> textures;
+	map<string, sf::Music*> music;
+	map<string, sf::Font*> fonts;
 
 };
 
--- src/resources-manager.cpp
+++ src/resources-manager.cpp
@@ -1,3 +1,47 @@
 
 #include "resources-manager.h"
-#include "common.h"
+
+ResourcesManager::ResourcesManager(){
+	//vacio.
+}
+
+void ResourcesManager::loadTextures(sf::Sprite& sprite, string name){
+ 	sf::Texture* objetotextura=0;
+
+	if(textures.count(name)>0)
+		objetotextura = textures[name];
+	
+	if(objetotextura==0){
+		cout << "Cargando imagen " << name << ".png" << endl;
+		objetotextura = new sf::Texture ();
+		string dir("data/textures/" + name + ".png");
+		if(!objetotextura->loadFromFile(dir))
+			cerr << "Error cargando imagen " << dir << endl;
+		textures[name]=objetotextura;
+	}
+
+	sprite.setTexture(*objetotextura, true);
+
+	sf::FloatRect bounds = sprite.getLocalBounds();
+	sprite.setOrigin(bounds.width/2, bounds.height/2);
+}
+
+ResourcesManager::~ResourcesManager(void){
+//Para texturas
+	{
+		typedef map<string, sf::Texture*>::const_iterator iterator;
+		for(iterator i = textures.begin(); i != textures.end(); ++i){
+			delete i->second;
+		}
+	}
+
+
+
+
+	/*cout << " * Deleting textures...";
+		typedef map<string, sf::Texture*>::const_iterator iterator;
+		for(iterator it = textures.begin(); it != textures.end(); ++it) {
+			delete it->second;
+		}
+		cout << "Done!" << endl;*/
+}
