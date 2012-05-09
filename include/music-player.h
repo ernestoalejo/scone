586e447d30d0370e65e5bee885a3ff1bfa4e399e
Alberto Moreno <alberto@alberto-SATELLITE-L755.(none)>
2012-05-09 20:23:55 +0200

    He subido la clase MusicPlayer.

--- include/music-player.h
+++ include/music-player.h
@@ -2,13 +2,29 @@
 #ifndef __MUSICPLAYER__H__
 #define __MUSICPLAYER__H__ 
 
-#include "commom.h"
+#include "common.h"
+#include <SFML/Audio.hpp>
+
 
 class MusicPlayer{
 private:
-	DISSALLOW_COPY_AND_ASSIGN(MusicPlayer);
-	<vector sf::Music*> 
+	DISALLOW_COPY_AND_ASSIGN(MusicPlayer);
+
+	vector <sf::Music*> music;
+	int current;
+	int size;
+	bool playing;
+	void next();
 public:
+	MusicPlayer();
+	~MusicPlayer();
+
+	void add(sf::Music& track);
+	void play();
+	void stop();
+	void update();
+
+	void chooseRandom();
 
 };
 
---
+++ src/music-player.cpp
@@ -0,0 +1,48 @@
+
+#include "music-player.h"
+
+MusicPlayer::MusicPlayer()
+: current(-1) , size(0) , playing(false)
+{
+}
+
+MusicPlayer::~MusicPlayer(){
+	// vacio
+}
+
+void MusicPlayer::add(sf::Music& track){
+	music[size] = &track;
+	size++;
+}
+
+void MusicPlayer::play(){
+	if (playing)
+		return;
+	playing = true;
+	next();
+}
+
+void MusicPlayer::next(){
+	current++;
+	if (current == size)
+		current = 0;
+	music[current]->play();
+}
+
+void MusicPlayer::stop(){
+	if(!playing)
+		return;
+	playing = false;
+	music[current]->stop();
+}
+
+void MusicPlayer::update(){
+	if(playing && music[current]->getStatus() == sf::SoundSource::Stopped)
+		next();
+}
+
+void MusicPlayer::chooseRandom(){
+	if(playing)
+		return;
+	current = rand() % size;
+}
