108f0816bfcd13f2c4afe04349e1831bfe02cbb7
Alberto Moreno <alberto@alberto-SATELLITE-L755.(none)>
2012-05-09 18:53:29 +0200

    Actualizacion sconstrict y sconstruct

---
+++ include/common.h
@@ -0,0 +1,22 @@
+
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// A macro to disallow the copy constructor and operator= functions
+// This should be used in the private: declarations for a class
+#define DISALLOW_COPY_AND_ASSIGN(TypeName) \
+  TypeName(const TypeName&);               \
+  void operator=(const TypeName&)
+
+const float SCREEN_WIDTH = 640;
+const float SCREEN_HEIGHT = 480;
+
+const float HALF_SCREEN_HEIGHT = SCREEN_HEIGHT / 2;
+const float HALF_SCREEN_WIDTH = SCREEN_WIDTH / 2;
+
+#endif
---
+++ include/music-player.h
@@ -0,0 +1,18 @@
+
+#ifndef __MUSICPLAYER__H__
+#define __MUSICPLAYER__H__ 
+
+#include "commom.h"
+
+class MusicPlayer{
+private:
+	DISSALLOW_COPY_AND_ASSIGN(MusicPlayer);
+	<vector sf::Music*> 
+public:
+
+};
+
+
+
+
+#endif
