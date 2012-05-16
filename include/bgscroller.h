9e6d8158255ee31b865a9c428c4f78bdcdd1ca4e
David <david@david-portatil.(none)>
2012-05-16 19:58:09 +0200

    Clase BgScroller(se encarga del fondo). No compila por que falta terminar el resources-manager.

---
+++ include/bgscroller.h
@@ -0,0 +1,30 @@
+#ifndef __BGSCROLLER_H__
+#define __BGSCROLLER_H__
+
+#include "common.h"
+#include "resources-manager.h"
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+class BgScroller :public sf::Drawable {
+
+private:
+	DISALLOW_COPY_AND_ASSIGN(BgScroller);
+	vector<sf::Sprite> sprites;
+	int current, size;
+	//Camera* camera;
+    float posx;
+	bool drawNext;
+	float bgWidth, halfBgWidth;
+
+public:
+	 BgScroller(ResourcesManager& manager, string name, int size);
+	~BgScroller();
+
+	void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+	void update(float diff);
+
+	//void follow(Camera& camera);
+};
+
+#endif
---
+++ src/bgscroller.cpp
@@ -0,0 +1,47 @@
+#include "resources-manager.h"
+#include "bgscroller.h"
+#include <sstream>
+
+BgScroller::BgScroller(ResourcesManager& manager, string name, int size)
+: size(size), posx(0), bgWidth(-1), sprites(size)
+{
+	for(int i = 0; i < size; i++) {
+		stringstream nombre;
+		nombre << name << "/" << i;
+		manager.loadTextures(sprites[i], nombre.str());
+
+		if(bgWidth < 0) {
+			bgWidth = sprites[i].getLocalBounds().width;
+			halfBgWidth = bgWidth / 2;
+		}
+
+		sprites[i].setPosition(i * bgWidth + halfBgWidth, HALF_SCREEN_HEIGHT);
+	}
+}
+
+BgScroller::~BgScroller() {
+	// vacio
+}
+
+void BgScroller::update(float diff) {
+	//posx = camera->getX();
+	posx+=0.001;
+
+	current = (posx / bgWidth);
+
+	drawNext = false;
+	if(sprites[current].getPosition().x + halfBgWidth < posx + SCREEN_WIDTH)
+		drawNext = true;
+}
+
+void BgScroller::draw(sf::RenderTarget& target, sf::RenderStates states) const {
+	target.draw(sprites[current]);
+	if(drawNext)
+		target.draw(sprites[current+1]);
+}
+/*
+
+void BgScroller::follow(Camera& camera) {
+	this->camera = &camera;
+
+	*/
