564343907b3e429a8698384c87dd13e9aa7c012e
Santiago <elwarxy@gmail.com>
2012-06-28 12:22:59 +0200

    Comentado bgscroller.h para doxygen.

--- include/bgscroller.h
+++ include/bgscroller.h
@@ -8,25 +8,83 @@
 #include <vector>
 #include "camera.h"
 
-
-class BgScroller : public sf::Drawable {
+/**
+ * Modulo para mover imagenes a partir del fondo.
+ */
+class BgScroller :public sf::Drawable {
 
 private:
 	DISALLOW_COPY_AND_ASSIGN(BgScroller);
+
+	/**
+	 * Vector de imagenes.
+	 */
 	vector<sf::Sprite> sprites;
-	int current, size;
+
+	/**
+	 * La imagen actual.
+	 */
+	int current;
+
+	/**
+	 * Tamaño de la imagen actual.
+	 */
+	int size;
+
+	/**
+	 * Carga la camara.
+	 */
 	Camera* camera;
+
+	/**
+	 * Guarda posicion horizontal.
+	 */
     float posx;
+
+    /**
+     * Indica si hay espacio para dibujar la siguiente imagen.
+     */
 	bool drawNext;
-	float bgWidth, halfBgWidth;
+
+	/**
+	 * Anchura de la imagen.
+	 */
+	float bgWidth;
+
+	/**
+	 * Mitad del ancho de la imagen.
+	 */
+	float halfBgWidth;
+
 
 public:
+
+	/**
+	 * Constructor.
+	 * @param manager Carga un recurso.
+	 * @param name Carga el nombre del recurso.
+	 * @param size Tamaño del recurso.
+	 */
 	 BgScroller(ResourcesManager& manager, string name, int size);
+
+	 /**
+	  * Destructor.
+	  */
 	~BgScroller();
 
+	/** @inheridDoc */
 	void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
+	/**
+	 * Actualiza fondo.
+	 * @param diff cantidad de segundos pasados tras el anterior fondo.
+	 */
 	void update(float diff);
 
+	/**
+	 * Asigna a la camara.
+	 * @param camera Camara a la que se le sigue.
+	 */
 	void follow(Camera& camera);
 };
 
